c0ac4710bbfccccc15eef6765e64439ec80cb4e0
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-01-30 15:20:17 +0100

    display products on html

--- products/src/index.js
+++ products/src/index.js
@@ -2,11 +2,9 @@ import faker from 'faker';
 
 let products = '';
 
-console.log('ksdjsjksksjsjkjk');
 
 for (let i = 0; i < 5; i++) {
     const name = faker.commerce.productName()
     products += `<div>${name}</div>`;
 }
-
-console.log(products);
+document.querySelector('#dev-products').innerHTML = products;
