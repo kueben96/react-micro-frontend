df1e55f99ae7b914f980e12dd43385afe9e5ec36
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-01-31 17:04:34 +0100

    22-share faker module between apps

--- products/src/index.js
+++ products/src/index.js
@@ -1,10 +1,2 @@
-import faker from 'faker';
-
-let products = '';
-
-
-for (let i = 0; i < 5; i++) {
-    const name = faker.commerce.productName()
-    products += `<div>${name}</div>`;
-}
-document.querySelector('#dev-products').innerHTML = products;
+// call import as function to asynch load modules
+import('./bootstrap');
--- products/webpack.config.js
+++ products/webpack.config.js
@@ -16,7 +16,8 @@ module.exports = {
             exposes: {
                 // aliases filenames (rename for import from container)
                 './ProductsIndex': './src/index'
-            }
+            },
+            shared: ['faker'],
         }),
         new HtmlWebpackPlugin({
             template: './public/index.html',
